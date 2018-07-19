288c1129ebc710c8a8fdd492e1b7b3d7064fb285
ochuko56 <kelvinekrresa@gmail.com>
2018-07-19 10:34:50 +0100

    implemented show/hide functionality to search button

--- js/validation.js
+++ js/validation.js
@@ -1,126 +1,140 @@
-// Checks if email is valid
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
+$(document).ready(function(){
 
-// *************Contact Form Validation*******************
-$('#contact').submit(function () {
+	// show search button on focus
+	$("input[type='search']").focus(function(){
+	    $("#search-btn").show(1000);
+	});
+	// hide search button on focus
+	$("input[type='search']").focusout(function(){
+	    $("#search-btn").hide(1000);
+	});
 
-	var errorMsg = '';
-	$('input').removeClass('form-error');
-	$('textarea').removeClass('form-error');
-
-	if ($('#name').val() == '') {
-		errorMsg += '<small>Please fill in your name</small><br>';
-		$('#name').addClass('form-error');
+	// Checks if email is valid
+	function isEmail(email) {
+	  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	  return regex.test(email);
 	}
 
-	if ($('#email').val() == '') {
-		errorMsg += '<small>Please fill in your email address</small><br>';
-		$('#email').addClass('form-error');
-	}
-	else if(!isEmail($('#email').val())) {
-		errorMsg += '<small>Your email is not valid</small><br>';
-		$('#email').addClass('form-error');
-	}
+	// *************Contact Form Validation*******************
+	$('#contact').submit(function () {
 
-	if ($('#subject').val() == '') {
-		errorMsg += '<small>The subject field is required</small><br>';
-		$('#subject').addClass('form-error');
-	}
+		var errorMsg = '';
+		$('input').removeClass('form-error');
+		$('textarea').removeClass('form-error');
 
-	if ($('#content').val() == '') {
-		errorMsg += '<small>The content field is required</small>';
-		$('#content').addClass('form-error');
-	}
+		if ($('#name').val() == '') {
+			errorMsg += '<small>Please fill in your name</small><br>';
+			$('#name').addClass('form-error');
+		}
+
+		if ($('#email').val() == '') {
+			errorMsg += '<small>Please fill in your email address</small><br>';
+			$('#email').addClass('form-error');
+		}
+		else if(!isEmail($('#email').val())) {
+			errorMsg += '<small>Your email is not valid</small><br>';
+			$('#email').addClass('form-error');
+		}
+
+		if ($('#subject').val() == '') {
+			errorMsg += '<small>The subject field is required</small><br>';
+			$('#subject').addClass('form-error');
+		}
+
+		if ($('#content').val() == '') {
+			errorMsg += '<small>The content field is required</small>';
+			$('#content').addClass('form-error');
+		}
 
 
-	if (errorMsg != '') {
-		$('#status').html(errorMsg).addClass('alert alert-danger text-muted');
-		return false;
-	}
-	else {
-		return true;
-	}
+		if (errorMsg != '') {
+			$('#status').html(errorMsg).addClass('alert alert-danger text-muted');
+			return false;
+		}
+		else {
+			return true;
+		}
+
+	});
+
+
+	// *************Registration Form Validation*******************
+	$('#registration').submit(function () {
+
+		var errorMsg = '';
+		$('input').removeClass('form-error');
+		$('textarea').removeClass('form-error');
+
+		if ($('#firstname').val() == '') {
+			errorMsg += '<small>Please fill in your first name</small><br>';
+			$('#firstname').addClass('form-error');
+		}
+
+		if ($('#lastname').val() == '') {
+			errorMsg += '<small>Please fill in your last name</small><br>';
+			$('#lastname').addClass('form-error');
+		}
+
+		if ($('#phone').val() == '') {
+			errorMsg += '<small>Please fill in your phone number</small><br>';
+			$('#phone').addClass('form-error');
+		}
+		else if (!$.isNumeric($('#phone').val())) {
+			errorMsg += '<small>Your phone number is not numeric</small><br>';
+			$('#phone').addClass('form-error');
+		}
+
+		if ($('#email').val() == '') {
+			errorMsg += '<small>Please fill in your email address</small><br>';
+			$('#email').addClass('form-error');
+		}
+		else if(!isEmail($('#email').val())) {
+			errorMsg += '<small>Your email is not valid</small><br>';
+			$('#email').addClass('form-error');
+		}
+
+		if ($('#password').val() == '') {
+			errorMsg += '<small>Please input a password</small><br>';
+			$('#password').addClass('form-error');
+		}
+
+		if ($('#confirmpassword').val() == '') {
+			errorMsg += '<small>Please confirm your password</small><br>';
+			$('#confirmpassword').addClass('form-error');
+		}
+
+		if ($('#password').val() !== $('#confirmpassword').val()) {
+			errorMsg += '<small>Your passwords don\'t match</small><br>';
+			$('#password').addClass('form-error');
+			$('#confirmpassword').addClass('form-error');
+		}
+
+
+		if (errorMsg != '') {
+			$('#status').html(errorMsg).addClass('alert alert-danger text-muted');
+			return false;
+		}
+		else {
+			return true;
+		}
+
+	});
+
+	// **********************Search Form Validation**********************
+	$('#search-form').submit(function () {
+		$("input[type='search']").removeClass('form-error');
+
+		if ($("input[type='search']").val() == '') {
+			$("input[type='search']").addClass('form-error');
+			alert('Please enter name of movie');
+			return false;
+		} else {
+			return true;
+		}
+
+	});
 
 });
 
 
-// *************Registration Form Validation*******************
-$('#registration').submit(function () {
-
-	var errorMsg = '';
-	$('input').removeClass('form-error');
-	$('textarea').removeClass('form-error');
-
-	if ($('#firstname').val() == '') {
-		errorMsg += '<small>Please fill in your first name</small><br>';
-		$('#firstname').addClass('form-error');
-	}
-
-	if ($('#lastname').val() == '') {
-		errorMsg += '<small>Please fill in your last name</small><br>';
-		$('#lastname').addClass('form-error');
-	}
-
-	if ($('#phone').val() == '') {
-		errorMsg += '<small>Please fill in your phone number</small><br>';
-		$('#phone').addClass('form-error');
-	}
-	else if (!$.isNumeric($('#phone').val())) {
-		errorMsg += '<small>Your phone number is not numeric</small><br>';
-		$('#phone').addClass('form-error');
-	}
-
-	if ($('#email').val() == '') {
-		errorMsg += '<small>Please fill in your email address</small><br>';
-		$('#email').addClass('form-error');
-	}
-	else if(!isEmail($('#email').val())) {
-		errorMsg += '<small>Your email is not valid</small><br>';
-		$('#email').addClass('form-error');
-	}
-
-	if ($('#password').val() == '') {
-		errorMsg += '<small>Please input a password</small><br>';
-		$('#password').addClass('form-error');
-	}
-
-	if ($('#confirmpassword').val() == '') {
-		errorMsg += '<small>Please confirm your password</small><br>';
-		$('#confirmpassword').addClass('form-error');
-	}
-
-	if ($('#password').val() !== $('#confirmpassword').val()) {
-		errorMsg += '<small>Your passwords don\'t match</small><br>';
-		$('#password').addClass('form-error');
-		$('#confirmpassword').addClass('form-error');
-	}
-
-
-	if (errorMsg != '') {
-		$('#status').html(errorMsg).addClass('alert alert-danger text-muted');
-		return false;
-	}
-	else {
-		return true;
-	}
-
-});
-
-// **********************Search Form Validation**********************
-$('#search-form').submit(function () {
-	$("input[type='search']").removeClass('form-error');
-
-	if ($("input[type='search']").val() == '') {
-		$("input[type='search']").addClass('form-error');
-		alert('Please enter name of movie');
-		return false;
-	} else {
-		return true;
-	}
-
-
-})
 
