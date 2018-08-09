e4c370b135b39e7b1f23464e48f8b8d08d63c4f3
ochuko56 <kelvinekrresa@gmail.com>
2018-08-09 20:51:06 +0100

    added db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+	/* Database connection settings */
+
+	$host = "db4free.net";
+	$username = "chuck_huey";
+	$password = "KyIkzt9mJRsFNkEq";
+	$dbname = "orion_accounts";
+
+	$conn = new mysqli($host, $username, $password,$dbname) or die($mysqli->error);
+
+?>
+
